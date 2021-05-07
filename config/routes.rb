2245008605a96820784551fc07b60b149b40aa1d
kento1101 <teshimaaaaa1101@gmail.com>
2021-05-07 06:59:45 +0000

    編集機能の追加

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'top'=>'homes#top'
+  get 'books'=>'books#index'
+  post 'books'=>'books#create'
+  
+  get 'books/:id'=>'books#show', as:'book'
+  get 'books/:id/edit'=>'books#edit',as:'edit_books'
+  patch 'books/:id'=>'books#update',as:'update'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
