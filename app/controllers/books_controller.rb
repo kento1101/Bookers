38521eb0fa3e19b330d0c5f322bb8b8c681ad669
kento1101 <teshimaaaaa1101@gmail.com>
2021-05-10 13:41:44 +0000

    完成！

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,10 +8,14 @@ def index
 end
 
 def create
-    book=Book.new(book_params)
-    book.save
-    redirect_to '/top'
-    
+     @books=Book.all
+     @book=Book.new(book_params)
+if   @book.save
+    redirect_to book_path(@book.id)
+    flash[:alert]="Book was successfully created."
+else
+     render :index
+end
 end
 
 def show
@@ -24,17 +28,23 @@ def edit
 end
 
 def update
-    book=Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book=Book.find(params[:id])
+  if   @book.update(book_params)
+    redirect_to book_path(@book.id)
+    flash[:notice]="Book was successfully updated."
+else
+    render :edit
     
+end
+   
+  
 end
 
 def destroy
    book=Book.find(params[:id])
    book.destroy
   redirect_to books_path
-    
+  flash[:message]="Book was successfully dested."      
 end
 
 
